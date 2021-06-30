ea64640399479ab5bb1425750fbc25851de023dd
Lovro Markovic <lovro.markovic@fer.hr>
2021-06-30 16:11:43 +0200

    Add run_carto_bag startup

---
+++ startup/run_carto_bag/custom_config/bag_setup_danieli2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export BAG_NAME=$HOME/Bags/carto_bags/carto_indoor_ouster.bag
+export CONFIGURATION_BASENAME=ouster_pozyx_old.lua
+export CONFIGURATION_DIRECTORY=$(pwd)/custom_config
+
+# Define bag topics
+export BAG_POINT_TOPIC=/os_cloud_node/points
+export BAG_IMU_TOPIC=/danieli2/mavros/imu/data
+export BAG_TRANSFORM_TOPIC=/pozyx/measured
+
+# Define cartographer topics
+export POINT_TOPIC=/$UAV_NAMESPACE/os_cloud_node/points
+export IMU_TOPIC=/$UAV_NAMESPACE//mavros/imu/data
+export TRANSFORM_TOPIC=/$UAV_NAMESPACE/pozyx/measured
